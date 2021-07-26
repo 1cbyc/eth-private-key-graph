16f5f53b27ca081bd0d3191cecc2b5d958aa7e8f
Isaac Nsisong <35623334+isaacnsisong@users.noreply.github.com>
2021-07-26 14:46:42 +0100

    Create main.cs

---
+++ main.cs
@@ -0,0 +1,35 @@
+// Created by Nsisong
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace NsisongETHkeyGen
+{
+    class Program
+    {
+        static string generatePrivKey(int length)
+        {
+            const string validChars = "abc12345def67890";
+            StringBuilder res = new StringBuilder();
+            Random rnd = new Random();
+            while (0 < length--)
+            {
+                res.Append(validChars[rnd.Next(validChars.Length)]);
+            }
+            return res.ToString();
+        }
+        
+        static void Main(string[] args)
+        {
+            //password length
+            int length = 64;
+            
+            string pass = generatePrivKey(length);
+            Console.Write(pass);
+        }
+    }
+}
